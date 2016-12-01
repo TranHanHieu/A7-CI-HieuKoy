fa9080ff2ef3bc68674f41cbab7034344ea34e95
TranHanHieu <hanhieu.a3.namly.2012@gmail.com>
2016-12-01 09:45:09 +0700

    first commit

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -11,10 +11,12 @@ import java.io.IOException;
  * Created by Hieu It on 11/30/2016.
  */
 public class GameWindow extends Frame {
+
     Image background;
-    Image plane;
+    Image plane, planeD1, planeD2, planeD4;
     private int planeX = 350;
     private int planeY = 400;
+    private int planeD1x = 300, planeD2x = 250, planeD4x = 500, planeD1y = 100, planeD2y = 500, planeD4y = 250;
 
     public GameWindow() {
 
@@ -58,13 +60,18 @@ public class GameWindow extends Frame {
 
             }
         });
+
         try {
             background = ImageIO.read(new File("resources/background.png"));
             plane = ImageIO.read(new File("resources/plane3.png"));
+            planeD1 = ImageIO.read(new File("resources/plane1.png"));
+            planeD2 = ImageIO.read(new File("resources/plane2.png"));
+            planeD4 = ImageIO.read(new File("resources/plane4.png"));
         } catch (IOException e) {
             System.out.println("Load Failed !");
             e.printStackTrace();
         }
+
         addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -76,19 +83,19 @@ public class GameWindow extends Frame {
                 System.out.println("keyPressed !");
                 switch (e.getKeyCode()) {
                     case KeyEvent.VK_UP:
-                        planeY-=5;
+                        planeY -= 5;
                         repaint();
                         break;
                     case KeyEvent.VK_DOWN:
-                        planeY+=5;
+                        planeY += 5;
                         repaint();
                         break;
                     case KeyEvent.VK_RIGHT:
-                        planeX+=5;
+                        planeX += 5;
                         repaint();
                         break;
                     case KeyEvent.VK_LEFT:
-                        planeX-=5;
+                        planeX -= 5;
                         repaint();
                         break;
                 }
@@ -107,5 +114,8 @@ public class GameWindow extends Frame {
     public void paint(Graphics g) {
         g.drawImage(background, 0, 0, 800, 600, null);
         g.drawImage(plane, planeX, planeY, 70, 50, null);
+        g.drawImage(planeD1, planeD1x, planeD1y, 70, 50, null);
+        g.drawImage(planeD2, planeD2x, planeD2y, 70, 50, null);
+        g.drawImage(planeD4, planeD4x, planeD4y, 70, 50, null);
     }
 }
